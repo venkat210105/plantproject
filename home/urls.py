fe9fc3461ecdde7c9a75f14882f82cb560af8399
venkat <venkatmariserla21@gmail.com>
2025-02-02 21:40:52 +0530

    Commit 1.1 for vercel

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Plant Detection App!")
 
 urlpatterns = [
     path("",views.index, name='home'),
--- plantproject/urls.py
+++ plantproject/urls.py
@@ -18,10 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to the Plant Detection App!")
 
 urlpatterns = [
     path('',include('home.urls')),
